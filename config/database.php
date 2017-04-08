7bde600a0b86ac03ba4ee5ed1172ec46012e4624
kenanEkici <kenan.ekici@outlook.com>
2017-04-08 18:21:52 +0200

    configuration remote database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '213.136.26.180'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'web07_db'),
+            'username' => env('DB_USERNAME', 'web07_db'),
+            'password' => env('DB_PASSWORD', 'web07'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
